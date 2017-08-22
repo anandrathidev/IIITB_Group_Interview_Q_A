e83fe70f7da8fac304abde75616566f3625b7c5e
AnandDev <anandrathi.dev@gmail.com>
2017-08-22 19:10:58 +0530

    Update UseFull_R_Commands_Functions.R

--- UseFull_R_Commands_Functions.R
+++ UseFull_R_Commands_Functions.R
@@ -1,1 +1,28 @@
 
+help() # Obtain documentation for a given R command
+example() #  View some examples on the use of a command
+c(), scan() # Enter data manually to a vector in R
+seq() #  Make arithmetic progression vector
+rep() # Make vector of repeated values
+data() # Load (often into a data.frame) built-in dataset
+View() # View dataset in a spreadsheet-type format
+str() # Display internal structure of an R object
+read.csv(), read.table() # Load into a data.frame an existing data file
+library(), require() # Make available an R add-on package
+dim() # See dimensions (# of rows/cols) of data.frame
+length() # Give length of a vector
+ls() #Lists memory contents
+rm() #Removes an item from memory
+names() #Lists names of variables in a data.frame
+hist() #Command for producing a histogram
+table() # List all values of a variable with frequencies
+xtabs() # Cross-tabulation tables using formulas
+cut() # Groups values of a variable into larger bins
+mean(), median() # Identify “center” of distribution
+by() # apply function to a column split by factors
+summary() # Display 5-number summary and mean
+var(), sd() # Find variance, sd of values in vector
+sum() # Add up all values in a vector
+quantile() # Find the position of a quantile in a dataset
+barplot() #Produces a bar graph
+boxplot() Produces a boxplot
